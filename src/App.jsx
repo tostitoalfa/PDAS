8cdd74245f74ccadbe6f8221997e6331b1e8890d
tostitoalfa <andrespdiaz1978@gmail.com>
2025-03-27 19:05:13 -0700

    modificado el componente profilecard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import ProfileCard from "./ProfileCard"
+import ProfileCard from './ProfileCard'
+
+import './App.css'
 
 function App() {
- 
 
   return (
     <>
-     <ProfileCard />
+      <section className="hero is-dark">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard 
+                title="Alexa"
+                handle="@alexa24"
+                image="https://cdn.shopify.com/s/files/1/0735/0540/7243/files/Alexa-Logo_480x480.svg?v=1713613419"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard />
+            </div>
+            <div className="column is-4">
+              <ProfileCard />
+            </div>
+          </div>
+        </section>
+      </div>
     </>
   )
 }
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,10 +1,21 @@
-function ProfileCard() {
+function ProfileCard({ title, handle, image }) {
 
     return (
-      <>
-       <div>ProfileCard</div>
-      </>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={image} alt="" width={200} height={200} />
+          </figure>
+        </div>
+        
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">Title is a {title}</p>
+            <p className="subtitle is-6">Handle is {handle}</p>
+          </div>
+        </div>
+      </div>
     )
-  }
-  
+}
+
 export default ProfileCard
